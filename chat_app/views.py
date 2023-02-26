831f341aaed16d80b0ce53a1ff9d7d2d38ca87cd
Maxim Manoilo <manoylo.maksimka@gmail.com>
2023-02-26 19:41:06 +0200

    Add Sign up

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    return render(request, 'chat_app/home.html')
 
 def chat(request):
     return render(request, 'chat_app/chat.html')
@@ -6,4 +13,16 @@ def chat(request):
 
 def room(request, room_name):
     return render(request, 'chat_app/room.html', {'room_name': room_name})
+
+def signupuser(requset):
+    if requset.method == 'GET':
+        return render(requset, 'chat_app/signup.html', {'form': UserCreationForm})
+    else:
+        if requset.POST['password1'] == requset.POST['password2']:
+            user = User.objects.create_user(username=requset.POST['username'], password=requset.POST['password1'])
+            user.save()
+            login(requset, user)
+            return redirect('home')
+
+
 
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from chat_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat_app.urls'))
+    path('', views.home, name='home'),
+    path('chat/', include('chat_app.urls')),
+
+    #Auth
+    path('signup/', views.signupuser, name='signupuser')
 ]
